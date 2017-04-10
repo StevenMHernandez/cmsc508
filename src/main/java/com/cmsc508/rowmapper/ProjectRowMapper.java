da3a1772458234dac02a7c93508ef45395650fc9
StevenMHernandez <contact@stevenhdesigns.com>
2017-04-10 17:09:50 -0400

    added project row mapper

---
+++ src/main/java/com/cmsc508/rowmapper/ProjectRowMapper.java
@@ -0,0 +1,24 @@
+package com.cmsc508.rowmapper;
+
+import com.cmsc508.model.Project;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProjectRowMapper implements RowMapper<Project> {
+    @Override
+    public Project mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Project project = new Project();
+        project.setId(rs.getLong("id"));
+        project.setName(rs.getString("name"));
+        project.setDescription(rs.getString("description"));
+        project.setStudentId(rs.getLong("studentId"));
+        project.setName(rs.getString("name"));
+        project.setDescription(rs.getString("description"));
+        project.setCreatedAt(rs.getDate("createdAt"));
+        project.setUpdatedAt(rs.getDate("updatedAt"));
+        return project;
+    }
+}
