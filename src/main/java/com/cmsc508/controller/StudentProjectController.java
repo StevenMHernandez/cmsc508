dc2528301aee079061fae297c040d066352326f9
Rya <rustgip@vcu.edu>
2017-05-05 21:19:11 -0400

    BIG PLAYS

--- src/main/java/com/cmsc508/controller/StudentProjectController.java
+++ src/main/java/com/cmsc508/controller/StudentProjectController.java
@@ -2,9 +2,11 @@ package com.cmsc508.controller;
 
 import com.cmsc508.model.Comment;
 import com.cmsc508.model.Project;
+import com.cmsc508.model.Star;
 import com.cmsc508.model.Student;
 import com.cmsc508.repository.CommentsRepository;
 import com.cmsc508.repository.ProjectsRepository;
+import com.cmsc508.repository.StarRepository;
 import com.cmsc508.repository.StudentsRepository;
 
 import org.springframework.http.MediaType;
@@ -30,12 +32,30 @@ public class StudentProjectController extends BaseController {
         return "students/projects/index";
     }
 
+    @RequestMapping(value="/student/{student_id}/projects/{project_id}/star", method=RequestMethod.POST)
+    public String storeStar(@PathVariable Integer project_id) {
+
+        new StarRepository(this.jdbcTemplate).insertStarred(this.getStudent().getId(), project_id);
+
+        return "redirect:/student/" + this.getStudent().getId() + "/projects";
+    }
+
+    @RequestMapping(value="/student/{student_id}/projects/", method=RequestMethod.GET)
+    public String showStar(@PathVariable Integer project_id) {
+
+        new StarRepository(this.jdbcTemplate).insertStarred(this.getStudent().getId(), project_id);
+
+        return "redirect:/student/" + this.getStudent().getId() + "/projects";
+    }
+
     @RequestMapping(value="/student/{student_id}/projects/{project_id}", method=RequestMethod.GET)
     public String show(@PathVariable Integer student_id, @PathVariable Integer project_id, Model model) {
         // get project data with comments and stars
         Project project = new ProjectsRepository(this.jdbcTemplate).get(project_id, student_id);
         List<Comment> comments = new CommentsRepository(this.jdbcTemplate).findForProject(project_id);
+        List<Star> stars = new StarRepository(this.jdbcTemplate).getStarred(student_id);
 
+        model.addAttribute("stars", stars);
         model.addAttribute("project", project);
         model.addAttribute("comments", comments);
         model.addAttribute("user", this.getStudent());
